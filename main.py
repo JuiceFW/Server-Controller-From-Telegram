889de11bcdfbddb7ba33f9e6558c93d6af84a766
JuiceFW <JuiceFW@yandex.ru>
2024-06-28 12:11:39 +0300

    Commands in terminal update!

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from telebot import types
+import telebot
 
 from multiprocessing.dummy import Process
+from typing import Union
 import subprocess
 import traceback
+import platform
 import logging
-import telebot
 import time
+import json
 import os
 
 from config import TOKEN, ADMIN_ID
@@ -56,7 +59,11 @@ markup = types.InlineKeyboardMarkup()
 item1 = types.InlineKeyboardButton("Удалить", callback_data='delete_message')
 markup.add(item1)
 
+
 def get_ip() -> str:
+    if platform.system() == "Windows":
+        return None
+
     output = None
     try:
         args = ["ip", "a"]
@@ -97,6 +104,19 @@ try:
 except:
     print(traceback.format_exc())
     logging.critical(traceback.format_exc())
+
+
+def get_commands_json() -> dict:
+    if not os.path.exists(os.path.join(BASE_DIR, "commands.json")):
+        return {"saved": [], "last": []}
+
+    with open(os.path.join(BASE_DIR, "commands.json"), encoding='utf-8') as file:
+        return json.loads(file.read())
+
+
+def write_commands_json(data: dict):
+    with open(os.path.join(BASE_DIR, "commands.json"), "w", encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False)
 
 
 def delete_in_time(message_id: int, chat_id: int, time_to_wait: int = 60) -> None:
@@ -193,7 +213,7 @@ def Welcome(message):
 
 
 @bot.callback_query_handler(func = lambda call: True)
-def callback_answer(call):
+def callback_answer(call: types.CallbackQuery):
     global turning_off
     global rebooting
 
@@ -260,15 +280,75 @@ def callback_answer(call):
         Process(target=reboot_in_time, args=(author_id, call.message.id, 10,)).start()
 
     elif call.data == "enter_command":
+        markup = types.InlineKeyboardMarkup()
+        item1 = types.InlineKeyboardButton("Ввести команду", callback_data="enter_command_text")
+        markup.row(item1)
+
+        commands = get_commands_json()
+        if commands.get("saved"):
+            item2 = types.InlineKeyboardButton("Сохраненные команды...", callback_data="saved_commands")
+            markup.row(item2)
+        if commands.get("last"):
+            item3 = types.InlineKeyboardButton("Последние команды...", callback_data="last_commands")
+            markup.row(item3)
+
+        item4 = types.InlineKeyboardButton("Меню", callback_data="start")
+        markup.row(item4)
+
+        try:
+            bot.delete_message(author_id, call.message.id)
+        except:
+            pass
+
+        bot.send_message(author_id, f"Выберите действие", reply_markup=markup)
+
+    elif call.data == "enter_command_text":
         msg = bot.edit_message_text(f"Введите команду или введите 'отмена': ", author_id, call.message.id)
         bot.register_next_step_handler(msg, after_command_enter, call.message.id)
+
+    elif call.data == "last_commands":
+        markup = types.InlineKeyboardMarkup()
+
+        commands = get_commands_json()
+
+        if len(commands.get("last")) < 10:
+            for val, item in enumerate(commands.get("last")):
+                btn = types.InlineKeyboardButton(item[:15], callback_data=f"cmd_{val}")
+                markup.row(btn)
+        else:
+            temp = []
+            for val, item in enumerate(commands.get("last")):
+                if len(temp) >= (2 if len(commands.get("last")) < 20 else 3):
+                    markup.row(*temp)
+                    temp = []
+
+                btn = types.InlineKeyboardButton(item[:15], callback_data=f"cmd_{val}")
+                temp.append(btn)
+
+            markup.row(*temp)
+            temp = []
+
+        item_last = types.InlineKeyboardButton("Меню", callback_data="start")
+        markup.row(item_last)
+
+        bot.edit_message_reply_markup(author_id, call.message.id, reply_markup=markup)
+
+    elif call.data.startswith("cmd_"):
+        index = int(call.data.replace("cmd_", ""))
+
+        commands = get_commands_json()
+        cmd = commands["last"][index]
+
+        call.message.text = cmd
+
+        return after_command_enter(call.message)
 
     elif call.data == "delete_message":
         bot.delete_message(author_id, call.message.id)
 
 
-def after_command_enter(msg, message_id):
-    if msg.text.lower() == "отмена":
+def after_command_enter(msg: types.Message, message_id: Union[int, None] = None):
+    if message_id and msg.text.lower() == "отмена":
         markup = types.InlineKeyboardMarkup()
         item1 = types.InlineKeyboardButton("Обратно в меню!", callback_data='start')
 
@@ -296,6 +376,17 @@ def after_command_enter(msg, message_id):
     # else:
     #     bot.edit_message_text(f"Ввод совершен успешно! Получаемые данные: [{code}]", msg.chat.id, message_id, reply_markup=markup)
 
+    commands_json = get_commands_json()
+    if not commands_json.get("last") or not isinstance(commands_json.get("last"), list):
+        commands_json["last"] = [msg.text]
+    else:
+        commands_json["last"] = [msg.text] + commands_json["last"]
+
+        if len(commands_json["last"]) > 24:
+            commands_json["last"] = commands_json["last"][:24]
+
+    write_commands_json(commands_json)
+
     got_error = None
     output = None
     try:
@@ -311,7 +402,10 @@ def after_command_enter(msg, message_id):
 
     markup.add(item1)
     if got_error:
-        bot.edit_message_text(f"Во время ввода произошла ошибка! Получаемые данные: [{got_error}]", msg.chat.id, message_id, reply_markup=markup)
+        if message_id:
+            bot.edit_message_text(f"Во время ввода произошла ошибка! Получаемые данные: [{got_error}]", msg.chat.id, message_id, reply_markup=markup)
+        else:
+            bot.send_message(msg.chat.id, f"Во время ввода произошла ошибка! Получаемые данные: [{got_error}]", reply_markup=markup)
     else:
         try:
             output = output.stdout.decode('utf-8')
@@ -319,7 +413,10 @@ def after_command_enter(msg, message_id):
             print(traceback.format_exc())
             logging.critical(traceback.format_exc())
         
-        bot.edit_message_text(f"Ввод совершен успешно! Получаемые данные:\n\n{output}", msg.chat.id, message_id, reply_markup=markup)
+        if message_id:
+            bot.edit_message_text(f"Ввод совершен успешно! Получаемые данные:\n\n{output}", msg.chat.id, message_id, reply_markup=markup)
+        else:
+            bot.send_message(msg.chat.id, f"Ввод совершен успешно! Получаемые данные:\n\n{output}", reply_markup=markup)
 
 
 while True:
